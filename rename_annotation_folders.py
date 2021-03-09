13b2a7f2848946fbcd6c4e4a3d9c3d68ecfaef69
pilarOrtega <48910147+pilarOrtega@users.noreply.github.com>
2021-03-09 15:49:27 +0100

    only change dir

--- rename_annotation_folders.py
+++ rename_annotation_folders.py
@@ -11,38 +11,39 @@ parser.add_argument("--infolder", type=Path)
 if __name__ == "__main__":
     args = parser.parse_args()
     for fold in tqdm(glob.glob(os.path.join(args.infolder, '*'))):
-        print(f'Changing folder {fold}')
-        for fn in Path(fold).iterdir():
-            if fn.is_dir():
-                new_name = fn.name.replace(" - ", "_")
-                new_name = new_name.replace(" ", "")
-                new_name = unidecode(new_name)
-                prop_file = fn/"Slidedat.ini"
-                try:
-                    with prop_file.open("r") as f:
-                        lines = f.read().split("\n")
-                except FileNotFoundError:
-                    print('Not a slide folder')
-                    break
-                lines.insert(3, "OBJECTIVE_MAGNIFICATION = 20")
-                line = lines.pop(2)
-                line = line.replace(" - ", "_")
-                line = unidecode(line)
-                lines.insert(2, line)
-                try:
+        if Path(fold).is_dir():
+            print(f'Changing folder {fold}')
+            for fn in Path(fold).iterdir():
+                if fn.is_dir():
+                    new_name = fn.name.replace(" - ", "_")
+                    new_name = new_name.replace(" ", "")
+                    new_name = unidecode(new_name)
+                    prop_file = fn/"Slidedat.ini"
+                    try:
+                        with prop_file.open("r") as f:
+                            lines = f.read().split("\n")
+                    except FileNotFoundError:
+                        print('Not a slide folder')
+                        break
+                    lines.insert(3, "OBJECTIVE_MAGNIFICATION = 20")
                     line = lines.pop(2)
-                    number = line.split(" ")[3]
-                    line = line.replace(f" {number}", f"{number}")
+                    line = line.replace(" - ", "_")
+                    line = unidecode(line)
                     lines.insert(2, line)
-                except IndexError:
+                    try:
+                        line = lines.pop(2)
+                        number = line.split(" ")[3]
+                        line = line.replace(f" {number}", f"{number}")
+                        lines.insert(2, line)
+                    except IndexError:
+                        with prop_file.open("w") as f:
+                            f.write("\n".join(lines))
+                        fn.replace(fn.parent/new_name)
+                        continue
                     with prop_file.open("w") as f:
                         f.write("\n".join(lines))
-                    fn.replace(fn.parent/new_name)
-                    continue
-                with prop_file.open("w") as f:
-                    f.write("\n".join(lines))
-            else:
-                new_name = fn.name.replace(" - ", "_")
-                new_name = new_name.replace(" ", "")
-                new_name = unidecode(new_name)
-            fn.replace(fn.parent/new_name)
+                else:
+                    new_name = fn.name.replace(" - ", "_")
+                    new_name = new_name.replace(" ", "")
+                    new_name = unidecode(new_name)
+                fn.replace(fn.parent/new_name)

088948b550c14ac600955015f02fd6968ea66c8c
Pilar <pilar.ortega.arevalo@gmail.com>
2021-05-04 14:21:40 +0200

    method to extract patch features

--- extract_features.py
+++ extract_features.py
@@ -1,34 +1,13 @@
 import os
-import pandas as pd
 import argparse
-from Pathaia_CNN_embedding import get_embeddings
-import pickle
 import tensorflow as tf
-
-
-def select_patches(patches, level, patch_size, slide_dir):
-    patch_list = []
-    patches = patches[patches['level'] == level]
-    for i, row in patches.iterrows():
-        slide_path = os.path.join(slide_dir, row['Diagnostic'])
-        slide_path = os.path.join(slide_path, row['Annotation'])+'.mrxs'
-        patch_list.append(
-            {'index': i,
-             'slide': slide_path,
-             'x': row['x'],
-             'y': row['y'],
-             'level': row['level'],
-             'dimensions': (patch_size, patch_size),
-             'true_label': row['Diagnostic']}
-        )
-    return patch_list
+from pathaia.util.management import util
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--patches', type=str)
+    parser.add_argument('--proj_dir', type=str)
     parser.add_argument('--slide_dir', type=str)
-    parser.add_argument('--level', type=int, default=0)
     parser.add_argument('--patch_size', type=int, default=224)
     parser.add_argument('--model', type=str, default='ResNet50')
     parser.add_argument('--layer', type=str, default='')
@@ -47,23 +26,16 @@ def main():
         except RuntimeError as e:
             print(e)
 
-    patches = args.patches
+    proj_dir = args.patches
     slide_dir = args.slide_dir
     level = args.level
     patch_size = args.patch_size
     model = args.model
     layer = args.layer
-    outdir = args.outdir
 
-    patches = pd.read_csv(patches, sep=None, engine='python')
-    patch_list = select_patches(patches, level, patch_size, slide_dir)
-    patch_list = dict(enumerate(patch_list))
-    descriptors = get_embeddings(model, patch_list, patch_size=patch_size, layer=layer)
-    for p in patch_list:
-        patch_list[p][f'Descriptor_{model}'] = descriptors[p]
-    if outdir != '':
-        with open(os.path.join(outdir, f'patches_{model}_level{level}.p'), 'wb') as f:
-            pickle.dump(patch_list, f)
+    handler = util.PathaiaHandler(proj_dir, slide_dir)
+    handler.extract_features(model=model, patch_size=patch_size, level=level,
+                             layer=layer)
 
 
 if __name__ == "__main__":
